aab0cfdec4f9cff40bcaf1b8b61f21ce72cf69bd
Keertan R <keertanr72@gmail.com>
2023-03-12 18:51:19 +0530

    pushingAgain

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const bodyParser = require('body-parser')
 
 const userRoutes = require('./routes/user')
 
+const errorController = require('./controllers/error')
+
 const app = express()
 app.use(cors())
 
 app.use(bodyParser.json({extended: false}))
 
 app.use('/user', userRoutes)
+
+app.use(errorController.getError)
 
 app.listen(3000)
