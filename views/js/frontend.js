bf01d5bff89ab2e142ddfadf11e3eae27afc9db0
Keertan R <keertanr72@gmail.com>
2023-03-14 00:59:10 +0530

    BackendAndFrontendOfSignup

--- views/js/frontend.js
+++ views/js/frontend.js
@@ -2,7 +2,6 @@ const createUser = async (event) => {
     let emailRepeat = false
     event.preventDefault()
     const check = await axios.get(('http://localhost:3000/user/details'))
-    alert('Submitted')
     check.data.forEach(data => {
                 if(data.email === event.target.email.value){
                     emailRepeat = true
@@ -10,10 +9,20 @@ const createUser = async (event) => {
                 }
     });
     if(emailRepeat){
-        document.getElementById('addHere').innerHTML = 'This email EXISTS!!'
+        document.getElementById('signUpFormId').innerHTML += '<p style="color: red;">This email EXISTS!!</p>'
         return
     }
 
+    if (!/[6-9]/.test(event.target.phoneNumber.value[0]) || event.target.phoneNumber.value.length !== 10) {
+        document.getElementById('signUpFormId').innerHTML += `<p style="color: red;">Enter a valid phone number</p>`;
+        return;
+    }
+
+    if(event.target.password.value.length < 7){
+        document.getElementById('signUpFormId').innerHTML += '<p style="color: red;">Password should contail atleast 6 digits!!</p>'
+        return
+    }
+    
     const userName = event.target.userName.value
     const email = event.target.email.value
     const phoneNumber = event.target.phoneNumber.value
@@ -29,6 +38,7 @@ const createUser = async (event) => {
     const data = await axios.post('http://localhost:3000/user/details', userDetails)
     try{
         if(data.status === 200){
+            alert('success')
             // window.location.href = "/views/html/login.html"
         }
     }
